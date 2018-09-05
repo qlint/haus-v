44cd1f10c1638589e80e6e9812da58f41a58db68
andy15la <andrew_urban@ymail.com>
2018-09-05 12:31:43 +0300

    New model for categorization.

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //we tell the model which table to use
+    protected $table = 'categories';
+
+    //we define the relationships ie one category will have many "items"
+    //aka one to many relationship
+
+    //I'll call my function "items"
+    public function items(){
+      //we're connecting it to the newitem model
+      return $this->hasMany('App\Newitem');
+    }
 }
