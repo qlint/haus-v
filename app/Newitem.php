a5be20ccd949339b93c5b1a6f3600111ad9e3385
andy15la <andrew_urban@ymail.com>
2018-09-14 09:33:25 +0300

    created categories model

--- app/Newitem.php
+++ app/Newitem.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Newitem extends Model
 {
-    //
+  //I'll call my function "category" because it belongs to one category
+  public function category(){
+    //note that 'belongsTo' assigns it to that model, in this case - category 
+    return $this->belongsTo('App\Category');
+  }
 }
